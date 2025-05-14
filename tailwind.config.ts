2085294823e75cbab23d6a6fe3cef9458f9b2db2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-05-14 09:49:58 -0400

    add daisy to tw config and add types

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        foreground: "var(--foreground)"
+      }
+    }
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: false
+  }
 } satisfies Config;
