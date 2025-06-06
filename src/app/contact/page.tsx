543dc198da2e1bdb6b73c5a4a882f38d5e2daea3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2025-06-06 17:32:28 -0400

    feat: add ContactPage component with ContactSection

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+//src/app/contact/page.tsx
+
+import ContactSection from "@/components/ContactSection";
+
+
+export default function ContactPage() {
+    return(
+        <div>
+<ContactSection />
+        </div>
+    );
+}
